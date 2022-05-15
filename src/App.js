683a90ede692bfb605c9b1033bc3173c2350c775
Lien-Chu Huang <liarn2001@gmail>
2022-05-15 15:40:03 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import {Welcome} from './myComponent';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Welcome name="Vicky"/><Welcome name="Vicky"/><Welcome name="Vicky"/><Welcome name="Vicky"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/myComponent.js
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+
+
+
+export class Welcome extends Component {
+    render() {
+      return <div><a href="https://google.com">what will it be?</a></div>
+    }
+  }
